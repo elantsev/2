5d5c13249add7666ee6f72770eeca7125b7c3b9a
Павел <elantsev.pa@gmail.com>
2018-10-14 09:41:53 +0300

    меняет цвета

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
-// Создайте страницу-калькулятор. 
-// Разместите на странице два поля ввода и кнопки для арифметических операций над данными
-// введенными в поля ввода. Реализуйте данную страницу двумя способами – с использованием
-// модели DOM Level 0 (через свойства) и DOM Level 2.
-
 // 1. захватываем элементы
-var buttons = document.getElementsByTagName("button");
+var body = document.getElementsByTagName("body");
+body[0].onkeypress = textColor;
 
 
 // 2. создаем функции
-function plus() {
-    var number1 = document.getElementsByTagName("input")[0].value;
-    var number2 = document.getElementsByTagName("input")[1].value;
-    var output = document.getElementsByTagName("p")[1];
-    output.innerHTML = parseFloat(number1)+parseFloat(number2)
+function textColor(e) {
+    console.log(e.charCode);
+    
+    
+    if (e.charCode==114) body[0].style.color = "red";
+    if (e.charCode==103) body[0].style.color = "green";
+    if (e.charCode==98) body[0].style.color = "blue";
 }
-function substraction() {
-    var number1 = document.getElementsByTagName("input")[0].value;
-    var number2 = document.getElementsByTagName("input")[1].value;
-    var output = document.getElementsByTagName("p")[1];
-    output.innerHTML = parseFloat(number1)-parseFloat(number2)
-}
-function multiply() {
-    var number1 = document.getElementsByTagName("input")[0].value;
-    var number2 = document.getElementsByTagName("input")[1].value;
-    var output = document.getElementsByTagName("p")[1];
-    output.innerHTML = parseFloat(number1)*parseFloat(number2)
-}
-function divide() {
-    var number1 = document.getElementsByTagName("input")[0].value;
-    var number2 = document.getElementsByTagName("input")[1].value;
-    var output = document.getElementsByTagName("p")[1];
-    output.innerHTML = (number2==0)? "на ноль делить нельзя" :parseFloat(number1)/parseFloat(number2);
-}
-// 3. вешаем события на кнопки DOM0
-// buttons[0].onclick = plus;
-// buttons[1].onclick = substraction;
-// buttons[2].onclick = multiply;
-// buttons[3].onclick = divide;
-// 4. вешаем события на кнопки DOM2
-buttons[0].addEventListener("click", plus);
-buttons[1].addEventListener("click", substraction);
-buttons[2].addEventListener("click", multiply);
-buttons[3].addEventListener("click", divide);
-
