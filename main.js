8de1fb2adce0678e8db59f8832615ff270d652f9
Павел <elantsev.pa@gmail.com>
2018-10-14 10:21:18 +0300

    12

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 // 1. захватываем элементы
 var body = document.getElementsByTagName("body");
-body[0].onkeypress = textColor;
+body[0].addEventListener("keypress", textColor);
 
 
 // 2. создаем функции
 function textColor(e) {
-    console.log(e.charCode);
-    
-    
     if (e.charCode==114) body[0].style.color = "red";
     if (e.charCode==103) body[0].style.color = "green";
     if (e.charCode==98) body[0].style.color = "blue";
