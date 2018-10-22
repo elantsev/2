d100994b4fb8b1ca7a541f7de47b480ab117b5ad
Павел <elantsev.pa@gmail.com>
2018-10-22 18:37:01 +0300

    модуль для аттрибута img у кнопки

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-var watermark={};
-watermark.element = form.login;
-watermark.element.value=watermark.element.getAttribute("watermark")
-watermark.element.className = "default"
-watermark.element.addEventListener("click",  function () {
-        if (watermark.element.value==watermark.element.getAttribute("watermark")) 
-       {watermark.element.value=""; 
-       watermark.element.className = "data";}
-})
-
-watermark.element.addEventListener("change",  function () {
-        if (watermark.element.value=="") watermark.element.value=watermark.element.getAttribute("watermark")
-        if (watermark.element.value==watermark.element.getAttribute("watermark")) 
-        watermark.element.className = "default"; // установка CSS класса 
-        else watermark.element.className = "data"; 
-})
 
 
-
-
-
-
-
+(function(){
+        const btn=document.getElementsByTagName("button");   
+          
+        for (let i = 0; i < btn.length; i++) {
+                btn[i].addEventListener("click", function () {
+                document.images[0].src=btn[i].attributes.img.value;
+                })} 
+        })()
+        
